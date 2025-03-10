436dd846ed3a08db07b1ce6bfdd1c5d91fba8c49
Agustín Perez <agperez.prieto@gmail.com>
2025-03-10 04:01:32 -0300

    Delete src/main/java/com/bronzeisunbreakable/minecraftconsole/Exception directory

--- src/main/java/com/bronzeisunbreakable/minecraftconsole/Exception/MalformedPacketException.java
+++
@@ -1,9 +0,0 @@
-package com.bronzeisunbreakable.minecraftconsole.exception;
-
-import java.io.IOException;
-
-public class MalformedPacketException extends IOException {
-    public MalformedPacketException(String message) {
-        super(message);
-    }
-}
